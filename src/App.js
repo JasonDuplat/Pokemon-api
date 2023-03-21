e1c00232f7f22a98ac62e7517e0c5bf7041e02bf
Jason Duplat <duplat.jason@outlook.com>
2023-03-21 17:45:05 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function Pokemon() {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+
+  const getPokemon = async () => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    setName(response.data.name);
+    setType(response.data.types[0].type.name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Find a Pokemon</h1>
+      <input
+        type="text"
+        placeholder="Enter Pokemon ID"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <button onClick={getPokemon}>Find</button>
+      {name && <p>Name: {name}</p>}
+      {type && <p>Type: {type}</p>}
     </div>
   );
 }
 
-export default App;
+export default Pokemon;
